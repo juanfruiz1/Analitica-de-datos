9467f626b1acb6df08a3e7347916d402514f5ec5
juanfruiz1 <juanf.ruiz1@udea.edu.co>
2026-03-30 22:02:49 -0500

    Programamos el setup_database y ejecutamos, la operación termina exitosamente y se crea en el directorio principal una carpeta (databse)ya que previamente no existía y se crea dentro de este directorio un proyecto_analitica.db que contiene las dos tablas

---
+++ src/setup_database.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import sqlite3
+import os
+
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+# 1. Definimos las rutas para evitar errores( vamos a crear la carpeta database si no existe)
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+
+DIR_ACTUAL = os.path.dirname(os.path.abspath(__file__))
+RUTA_DB = os.path.join(DIR_ACTUAL, "../database/proyecto_analitica.db")
+
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+# Rutas de los archivos CSV originales:
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+
+RUTA_BPN = os.path.join(DIR_ACTUAL, "../data/raw/BPN.csv")
+RUTA_NHANES = os.path.join(DIR_ACTUAL, "../data/raw/NHANES.csv")
+
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+#creamos la función para la creación de la base de datos
+#⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+
+def crear_base_datos():
+    
+    #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+    # 2. conectamos a SQLite (si el archivo no existe, lo crea automáticamente)
+    #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+    
+    conexion = sqlite3.connect(RUTA_DB)
+    
+    try:
+        #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+        # 3. Cargar los CSV en Pandas
+        #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+        
+        df_bpn = pd.read_csv(RUTA_BPN, sep=';') # Ajusta el 'sep' si el CSV usa comas (',')#
+        
+        df_nhanes = pd.read_csv(RUTA_NHANES, sep=',')
+        
+        #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+        # 4. Enviamos los datos a SQLite para crear las tablas
+        #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+        
+        df_bpn.to_sql('regresion_bpn', conexion, if_exists='replace', index=False)
+        
+        df_nhanes.to_sql('clasificacion_nhanes', conexion, if_exists='replace', index=False)
+        
+        #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+        # Mensaje puramente estético
+        #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+        
+        print("✅ ¡Éxito! Base de datos y tablas creadas correctamente en database/proyecto_analitica.db")
+        
+    #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+    # En caso de error
+    #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+    
+    except Exception as e:
+        print(f"❌ Ocurrió un error: {e}")
+        
+    finally:
+        
+        #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+        # Siempre tenemos que cerramos la conexión
+        #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+        
+        conexion.close()
+
+if __name__ == "__main__":
+    
+    #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+    # Crear la carpeta database si por casualidad no existe
+    #⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘#
+    
+    os.makedirs(os.path.join(DIR_ACTUAL, "../database"), exist_ok=True)
+    crear_base_datos()
